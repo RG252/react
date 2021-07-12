1a8b172b91a507efb9ff5246fac88066eee9cacf
Rohan Gupta <guptarohan2012@gmail.com>
2021-07-12 10:47:42 +0530

    component life cycle methods

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import User from "./components/user";
+
+//JSX
+function App() {
+  return(
+    <div>
+      <h1>Users</h1>
+      <User name="Rohan" description="FSWD" />
+    </div>
+  );
+}
+
+export default App;
